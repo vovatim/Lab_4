d3cdfdf7f628313738d090b4e28ab2fdd7dc5e1a
Grigory Ozhegov <grigory@ozhegov.name>
2016-10-12 05:37:37 +0300

    fix description

--- librip/gen.py
+++ librip/gen.py
@@ -7,8 +7,8 @@ import random
 #    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
 #    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'}
 # ]
-# field(goods, 'title') должен выдавать ['Ковер', 'Диван для отдыха']
-# field(goods, 'title', 'price') должен выдавать [{'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}]
+# field(goods, 'title') должен выдавать 'Ковер', 'Диван для отдыха'
+# field(goods, 'title', 'price') должен выдавать {'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}
 
 def field(items, *args):
     assert len(args) > 0
