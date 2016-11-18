47c61d5ad63c2a3f0191b6222134ea7f89f97145
vovatim <vovatim1990@gmail.com>
2016-11-18 14:06:34 +0300

    Update and rename gens.py to gen.py

--- librip/gen.py
+++ librip/gen.py
@@ -12,7 +12,18 @@ import random
 
 def field(items, *args):
     assert len(args) > 0
-    # Необходимо реализовать генератор 
+    # Необходимо реализовать генератор
+    for elem in items:
+        r = {}
+        for arg in args:
+            if arg in elem:
+                if len(args) == 1:
+                    yield elem[arg]
+                else:
+                    if elem[arg] is not None:
+                        r[arg] = elem[arg]
+        if len(r) > 0 and len(args) > 1:
+            yield r
 
 
 # Генератор списка случайных чисел
@@ -20,5 +31,7 @@ def field(items, *args):
 # gen_random(1, 3, 5) должен выдать примерно 2, 2, 3, 2, 1
 # Hint: реализация занимает 2 строки
 def gen_random(begin, end, num_count):
-    pass
     # Необходимо реализовать генератор
+    i = 1
+    for i in range(num_count):
+        yield random.randint(begin, end)
