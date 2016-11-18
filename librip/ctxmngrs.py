1d8bb32f02dfbfb1080b73716d3e382f28fe0847
vovatim <vovatim1990@gmail.com>
2016-11-18 14:05:39 +0300

    Update ctxmngrs.py

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -6,3 +6,13 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+import time
+
+class timer:
+    def __enter__(self):
+        self.start = (time.time())
+
+    def __exit__(self, exp_type, exp_value, traceback):
+        tstop = (time.time())
+        tstop -= self.start
+        print("Время выполнения: " + str(tstop))
