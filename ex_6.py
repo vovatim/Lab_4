72695aad84ab07f089006211b121eb787fccd126
vovatim <vovatim1990@gmail.com>
2016-11-18 13:59:58 +0300

    Update ex_6.py

--- ex_6.py
+++ ex_6.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python3
 import json
 import sys
 from librip.ctxmngrs import timer
 from librip.decorators import print_result
-from librip.gens import field, gen_random
+from librip.gen import *
 from librip.iterators import Unique as unique
 
-path = None
+path = "data_light.json"
 
 # Здесь необходимо в переменную path получить
 # путь до файла, который был передан при запуске
@@ -23,22 +22,24 @@ with open(path) as f:
 
 @print_result
 def f1(arg):
-    raise NotImplemented
+    return list(unique(list(field(arg, "job-name")), ignore_case=True))
 
 
 @print_result
 def f2(arg):
-    raise NotImplemented
+    return list(filter(lambda _: "Программист" in _, arg))
 
 
 @print_result
 def f3(arg):
-    raise NotImplemented
+    return list(map(lambda x: x + " с опытом Python", arg))
 
 
 @print_result
 def f4(arg):
-    raise NotImplemented
+    random_salaries = gen_random(100000, 200000, len(arg))
+    zipped_workers_with_salaries = zip(arg, random_salaries)
+    return list(map(lambda x: "{}, зарплата {} руб.".format(x[0], x[1]), zipped_workers_with_salaries))
 
 
 with timer():
