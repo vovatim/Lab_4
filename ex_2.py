5a7e01769a8f248ba4dd933fb58f595ebf25ec1d
vovatim <vovatim1990@gmail.com>
2016-11-18 13:58:29 +0300

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python3
-from librip.gens import gen_random
+from librip.gen import gen_random
 from librip.iterators import Unique
 
-data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+data1 = [1, 1, 1, 1, 1, 2, 2, 3,3,3, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
 
 # Реализация задания 2
+print(list(Unique(data1)))
+print(list(Unique(data2)))
+data = ['a', 'A', 'b', 'B']
+print(list(Unique(data)))
+data = ['a', 'A', 'B', 'b']
+print(list(Unique(data, ignore_case=True)))
